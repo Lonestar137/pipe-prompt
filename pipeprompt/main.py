5981f711d08b29b6308f2642255b259840103cc9
Garrett Jones <garrettcj2010@hotmail.com>
2023-01-22 12:02:15 -0600

    fixed a bug with processing row data.

--- pipeprompt/main.py
+++ pipeprompt/main.py
@@ -1,8 +1,9 @@
-from pipeprompt.src.args import args_core
-from pipeprompt.src.stdin import standard_core
+from typing import List
 from argparse import ArgumentParser
 from pipeprompt.src.constants import CWD
-from typing import Tuple
+from pipeprompt.src.args import args_core
+from pipeprompt.src.classes.row import Row
+from pipeprompt.src.stdin import standard_core
 
 
 def main():
@@ -14,12 +15,14 @@ def main():
 
         std_input: str = ""
         if args.debug:
-            # std_input: str = "hello_world@#cmd: testing command #@"
-            std_input: str = "hello_world"
+            # std_input: str = "hello_world@#color: testing command #@"
+            std_input: str = "hello_world@#cmd: testing command #@"
+            # std_input: str = "hello_world"
         else:
             std_input: str = standard_core.collect_pipe_input()
 
-        standard_core.entry_point(std_input, args)
+        rows: List[Row] = standard_core.entry_point(std_input, args)
+
     except KeyboardInterrupt:
         print("Process closed due to keyboard interrupt.")
 
